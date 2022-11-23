639c717ff72019400c0a3668d713a45a575700fb
imaimai17468 <tosiki_801_life@outlook.jp>
2022-11-24 01:15:37 +0900

    [style]formatにより整形(#4)

--- view/src/pages/index.tsx
+++ view/src/pages/index.tsx
@@ -30,9 +30,7 @@ export default function Home() {
   }
 
   const prefectureButtons = prefectures.map((prefecture: PrefectureProps) => (
-    <ToggleButton
-      onClick={() => handleSelectPrefecture(prefecture)}
-    >
+    <ToggleButton onClick={() => handleSelectPrefecture(prefecture)}>
       {prefecture.prefName}
     </ToggleButton>
   ))
