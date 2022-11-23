24120cf0c6351e6da7a299a5dc102c8be6314763
imaimai17468 <tosiki_801_life@outlook.jp>
2022-11-24 00:48:30 +0900

    [feat]都道府県を表示(#4)

--- view/src/pages/index.tsx
+++ view/src/pages/index.tsx
@@ -1,9 +1,51 @@
-import { Button, Card } from '@components/common'
+import { ToggleButton, Card } from '@components/common'
+import { usePrefectures } from '@hooks/usePrefectures'
+import style from '@styles/index.module.scss'
+import { useState, useEffect } from 'react'
+
+interface PrefectureProps {
+  prefCode: number
+  prefName: string
+}
 
 export default function Home() {
+  const prefectures = usePrefectures()
+  const [selectedPrefectures, setSelectedPrefectures] =
+    useState<PrefectureProps[]>()
+
+  useEffect(() => {}, [selectedPrefectures])
+
+  const handleSelectPrefecture = (prefecture: PrefectureProps) => {
+    if (
+      selectedPrefectures?.find((pref) => pref.prefCode === prefecture.prefCode)
+    ) {
+      setSelectedPrefectures(
+        selectedPrefectures.filter(
+          (pref) => pref.prefCode !== prefecture.prefCode
+        )
+      )
+    } else {
+      setSelectedPrefectures([...(selectedPrefectures || []), prefecture])
+    }
+  }
+
+  const prefectureButtons = prefectures.map((prefecture: PrefectureProps) => (
+    <ToggleButton
+      key={prefecture.prefCode}
+      onClick={() => handleSelectPrefecture(prefecture)}
+    >
+      {prefecture.prefName}
+    </ToggleButton>
+  ))
+
   return (
     <div>
-      <Card>test</Card>
+      <Card>
+        <div>
+          <h2 className={style.title}>Prefectures</h2>
+          <div className={style.prefecture_container}>{prefectureButtons}</div>
+        </div>
+      </Card>
     </div>
   )
 }
