bad819511725707535d9f7eddefeccce392bcc37
imaimai17468 <tosiki_801_life@outlook.jp>
2022-11-26 00:18:08 +0900

    [del]デバッグ文を削除(#7)

--- view/src/api/getPrefectures.ts
+++ view/src/api/getPrefectures.ts
@@ -7,7 +7,6 @@ interface Prefecture {
 
 export const getPrefectures = async () => {
   const res = await prefecturesAPI.get('')
-  console.log(res.data)
   const prefectures = res.data.result.map((prefecture: Prefecture) => ({
     prefCode: prefecture.prefCode,
     prefName: prefecture.prefName,
